5fb412fd6491fa0c9ff03417fb3e72edf4e83f45
accgit <zdenek.papucik@gmail.com>
2026-01-03 10:12:22 +0100

    Fix

--- assets/sidebar.menu.js
+++ assets/sidebar.menu.js
@@ -1,6 +1,14 @@
 import "./sidebar.skeleton";
-import "perfect-scrollbar";
-import "nanobar";
+import PerfectScrollbar from "perfect-scrollbar";
+import Nanobar from "nanobar"
 import "sidebar-menu-compostrap";
 
 import "./sidebar.menu.scss";
+
+/* Initialize components after DOM is loaded */
+document.addEventListener("DOMContentLoaded", () => {
+	new Nanobar().go(100);
+	new PerfectScrollbar('.scrollbar', {
+		wheelSpeed: 0.3
+	});
+});
